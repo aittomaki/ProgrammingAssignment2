375f560ca3cf18e60ad5a38ced575439bd110416
aittomaki <aittomaki@users.noreply.github.com>
2014-12-21 23:48:35 +0200

    Added comments describing functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions for matrices that have a cached inverse matrix.
+## Allows using cached values for matrix inversions so they don't have
+## to be recalculated every time an inverse is needed.
+## This is a solution to Programming Assignment 2 of the Coursera
+## course R Programming.
+##
+## Author: Viljami Aittomaki
 
-## Write a short comment describing this function
+
+## Creates a special matrix, that is a list of four functions:
+##     set sets the value of stored the matrix
+##     get returns the value of the matrix
+##     setinverse sets the inverse for the matrix (shouldn't be used directly)
+##     getinverse returns the inverse of the stored matrix
 
 makeCacheMatrix <- function(x = matrix()) {
     # Create a variable for storing the inverse
@@ -25,15 +35,20 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+
+## Solves the inverse for a given cacheMatrix (created with
+## the createCacheMatrix function) and caches it.
+## If the inverse has already been solved and cached,
+## returns the cached value.
 
 cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
+    # Get the inverse from x and return if it is defined
     i <- x$getinverse()
     if(!is.null(i)) {
         message("Returning cached inverse.")
         return(i)
     }
+    # Solve and cache the inverse if it hasn't been defined
     data <- x$get()
     i <- solve(data, ...)
     x$setinverse(i)
