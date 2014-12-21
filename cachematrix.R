0effaf1eacf773a93eb003abd237abca020823ef
aittomaki <aittomaki@users.noreply.github.com>
2014-12-21 23:18:13 +0200

    Added function for creating a cache matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # Create a variable for storing the inverse
+    i <- NULL
+    # Function for setting the value of the matrix
+    set <- function(y) {
+        x <<- y
+        # Reset the inverse
+        i <<- NULL
+    }
+    # Function for getting the matrix itself
+    get <- function() x
+    # Function for setting the inverse of the matrix
+    setinverse <- function(y) i <<- y
+    # Function for getting the inverse
+    getinverse <- function() i
+    # Return a list with the functions
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
 }
